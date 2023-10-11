e589592115162806ef70481a0d990a8f671d566a
Bryan Herrera <bryan.herrera@chargepoint.com>
2023-10-11 11:35:38 -0700

    first commit

---
+++ fxnl.py
@@ -0,0 +1,36 @@
+import serial
+import time
+
+usb_port_name = "/dev/tty.usbserial-0001"
+
+serialPort = serial.Serial(
+    port=usb_port_name, baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE
+)
+
+print("Starting test...\n")
+serialPort.write(b'l 0 0 0\r')
+
+time.sleep(1)
+
+print("Turn LED Red\n")
+serialPort.write(b'l 0\r')
+
+time.sleep(2)
+
+print("Turn LED Green\n")
+serialPort.write(b'l 120\r')
+
+time.sleep(2)
+
+print("Turn LED Blue\n")
+serialPort.write(b'l 240\r')
+
+time.sleep(2)
+
+print("Turn LED White (All LED On)\n")
+serialPort.write(b'l 50 50 50\r')
+
+time.sleep(2)
+
+print("Functional Test Completed\n")
+serialPort.write(b'l 0 0 0\r')
